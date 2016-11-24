2ff08866f4fcf891e9fccbcb8d1066b6f02623bc
SergioSkateder <sergioskateder@gmail.com>
2016-11-23 19:08:40 -0600

    Initial commit

---
+++ app/src/main/java/com/herprogramacion/janusshop/ProductosFragment.java
@@ -0,0 +1,14 @@
+package com.herprogramacion.janusshop;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductosFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_productos_fragment);
+    }
+}
